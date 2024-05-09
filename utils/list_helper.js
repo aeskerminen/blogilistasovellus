ad2896fc3ee4d17a4b315c1414f1a85312204f34
Artturi <kerminenartturi@gmail.com>
2024-05-09 19:03:48 +0300

    4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,11 @@
 const dummy = (blogs) => {
-    return 1;
-  }
-  
-  module.exports = {
-    dummy
-  }
+  return 1;
+};
+
+const totalLikes = (blogs) => {
+    return blogs.reduce((acc, cur) => acc + cur.likes, 0);
+}
+
+module.exports = {
+  dummy, totalLikes
+};
