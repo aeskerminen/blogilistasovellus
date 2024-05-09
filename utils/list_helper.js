fb1098ca366f26902a0b7e12ce12a134d1c9c4fb
Artturi <kerminenartturi@gmail.com>
2024-05-09 19:18:22 +0300

    4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,6 +6,14 @@ const totalLikes = (blogs) => {
     return blogs.reduce((acc, cur) => acc + cur.likes, 0);
 }
 
+const favoriteBlog = (blogs) => {
+    if(blogs.length <= 0)
+        return ""
+
+    const sorted = [...blogs].sort((a,b) => b.likes - a.likes)
+    return sorted[0]._id
+}
+
 module.exports = {
-  dummy, totalLikes
+  dummy, totalLikes, favoriteBlog
 };
